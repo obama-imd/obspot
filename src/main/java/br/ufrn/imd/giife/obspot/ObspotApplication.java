8a5a4e518641cbd3dc546d470570badab44c163f
Emanuel Kywal <emanuelkywal@outlook.com>
2025-07-03 22:12:32 -0300

    first commit

---
+++ src/main/java/br/ufrn/imd/giife/obspot/ObspotApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.giife.obspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/imd/giife/obspot/ObspotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.giife.obspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
