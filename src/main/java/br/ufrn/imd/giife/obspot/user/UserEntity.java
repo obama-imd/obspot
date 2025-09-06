0081eb9b86330b028b19bbbb6a3d3dcd1921b73f
Emanuel Kywal <emanuelkywal@outlook.com>
2025-09-06 19:16:01 -0300

    feat: UserEntity class

---
+++ src/main/java/br/ufrn/imd/giife/obspot/user/UserEntity.java
@@ -0,0 +1,50 @@
+package br.ufrn.imd.giife.obspot.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
