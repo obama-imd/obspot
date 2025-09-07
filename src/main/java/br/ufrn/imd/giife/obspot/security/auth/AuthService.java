4c34fb26a294ce0ad2394b9a6e783699bd2fc17b
Emanuel Kywal <emanuelkywal@outlook.com>
2025-09-07 02:09:35 -0300

    feat: adds password enconding in user registration

--- src/main/java/br/ufrn/imd/giife/obspot/security/auth/AuthService.java
+++ src/main/java/br/ufrn/imd/giife/obspot/security/auth/AuthService.java
@@ -6,6 +6,7 @@ import br.ufrn.imd.giife.obspot.user.service.UserService;
 import br.ufrn.imd.giife.obspot.security.auth.controller.dto.RegistrationRequestDTO;
 import br.ufrn.imd.giife.obspot.security.jwt.JwtService;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,22 +15,27 @@ public class AuthService {
     private final JwtService jwtService;
     private final UserService userService;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     AuthService(
             JwtService jwtService,
             UserService userService,
-            UserMapper userMapper
+            UserMapper userMapper,
+            PasswordEncoder passwordEncoder
     ) {
         this.jwtService = jwtService;
         this.userService = userService;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public UserEntity register(RegistrationRequestDTO newUser) {
-        checkPasswords(newUser.password(), newUser.confirmPassword());
-        return userService.save(
-                userMapper.toEntity(newUser)
-        );
+    public UserEntity register(RegistrationRequestDTO registrationDTO) {
+        checkPasswords(registrationDTO.password(), registrationDTO.confirmPassword());
+
+        UserEntity newUser = userMapper.toEntity(registrationDTO);
+        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+
+        return userService.save(newUser);
     }
 
     private void checkPasswords(final String password, final String confirmPassword) {
