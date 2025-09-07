38cbed283fec8987d71610be7d2a4326ca4e1921
Emanuel Kywal <emanuelkywal@outlook.com>
2025-09-07 00:07:28 -0300

    feat: SecurityConfig class with beans for SecurityFilterChain, AuthenticationManager, and BCryptPasswordEncoder

---
+++ src/main/java/br/ufrn/imd/giife/obspot/user/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package br.ufrn.imd.giife.obspot.user.security;
+
+import br.ufrn.imd.giife.obspot.user.security.jwt.JwtAuthFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    SecurityConfig(
+            JwtAuthFilter jwtAuthFilter
+    ) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authenticationManager(authManager)
+                .authorizeHttpRequests(
+                        authRegistry -> authRegistry
+                                .requestMatchers("/auth/**").permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
