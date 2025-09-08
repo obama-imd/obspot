5ab6746f1835d3379ee1f5a629ba51664d4f87d7
Emanuel Kywal <emanuelkywal@outlook.com>
2025-09-07 23:29:55 -0300

    feat: methods login and authenticate on AuthService

--- src/main/java/br/ufrn/imd/giife/obspot/security/auth/AuthService.java
+++ src/main/java/br/ufrn/imd/giife/obspot/security/auth/AuthService.java
@@ -1,28 +1,37 @@
 package br.ufrn.imd.giife.obspot.security.auth;
 
+import br.ufrn.imd.giife.obspot.security.auth.controller.dto.LoginRequestDTO;
+import br.ufrn.imd.giife.obspot.security.auth.controller.dto.LoginResponseDTO;
+import br.ufrn.imd.giife.obspot.security.auth.controller.dto.RegistrationRequestDTO;
+import br.ufrn.imd.giife.obspot.security.jwt.JwtService;
+import br.ufrn.imd.giife.obspot.security.userdetails.UserDetailsImpl;
 import br.ufrn.imd.giife.obspot.user.UserEntity;
 import br.ufrn.imd.giife.obspot.user.controller.UserMapper;
 import br.ufrn.imd.giife.obspot.user.service.UserService;
-import br.ufrn.imd.giife.obspot.security.auth.controller.dto.RegistrationRequestDTO;
-import br.ufrn.imd.giife.obspot.security.jwt.JwtService;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
+    private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final UserService userService;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
 
     AuthService(
+            AuthenticationManager authenticationManager,
             JwtService jwtService,
             UserService userService,
             UserMapper userMapper,
             PasswordEncoder passwordEncoder
     ) {
+        this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
         this.userService = userService;
         this.userMapper = userMapper;
@@ -36,6 +45,24 @@ public class AuthService {
         newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
 
         return userService.save(newUser);
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO loginDTO) {
+        Authentication auth = authenticate(loginDTO);
+
+        UserDetailsImpl user = (UserDetailsImpl) auth.getPrincipal();
+        String accessToken = jwtService.generateToken(user.getUsername());
+
+        return new LoginResponseDTO(accessToken);
+    }
+
+    public Authentication authenticate(LoginRequestDTO loginDTO) {
+        return authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDTO.email(),
+                        loginDTO.password()
+                )
+        );
     }
 
     private void checkPasswords(final String password, final String confirmPassword) {
