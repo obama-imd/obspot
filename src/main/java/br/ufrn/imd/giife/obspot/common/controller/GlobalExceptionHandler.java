c1a1b5c05d0bc12ae8a55735bcb9df31d2f3bd2e
Emanuel Kywal <emanuelkywal@outlook.com>
2025-09-07 23:26:44 -0300

    feat: GlobalExceptionHandler and handleGenericException method

---
+++ src/main/java/br/ufrn/imd/giife/obspot/common/controller/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package br.ufrn.imd.giife.obspot.common.controller;
+
+import br.ufrn.imd.giife.obspot.common.controller.dto.ResponseDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<ResponseDTO> handleGenericException(Throwable ex) {
+        log.error(ex.getMessage(), ex);
+
+        return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(
+                        new ResponseDTO(
+                                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                                "Erro no servidor"
+                        )
+                );
+    }
+
+}
