026d0810e21bfdc63055a154e0b7b4230d806fa3
Emanuel Kywal <emanuelkywal@outlook.com>
2025-09-09 15:52:03 -0300

    feat: method UserService.findById
    adds exception messages and makes them public

--- src/main/java/br/ufrn/imd/giife/obspot/user/UserService.java
+++ src/main/java/br/ufrn/imd/giife/obspot/user/UserService.java
@@ -1,15 +1,19 @@
 package br.ufrn.imd.giife.obspot.user;
 
 import br.ufrn.imd.giife.obspot.common.service.exception.EntityAlreadyExistsException;
+import br.ufrn.imd.giife.obspot.common.service.exception.EntityNotFoundException;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class UserService {
 
-    private static final String USER_NOT_FOUND_MESSAGE = "User %s not found";
-    private static final String USER_ALREADY_EXISTS_MESSAGE = "There's already a user with the email %s";
+    public static final String USER_ID_NOT_FOUND_MESSAGE = "Usuário de ID %s não encontrado";
+    public static final String USER_EMAIL_NOT_FOUND_MESSAGE = "Usuário %s não encontrado";
+    public static final String USER_ALREADY_EXISTS_MESSAGE = "Já existe um usuário com o email %s";
 
     private final UserRepository userRepository;
 
@@ -24,9 +28,14 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public UserEntity findById(@NonNull UUID id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(USER_ID_NOT_FOUND_MESSAGE.formatted(id)));
+    }
+
     public UserEntity findByEmail(@NonNull String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND_MESSAGE.formatted(email)));
+                .orElseThrow(() -> new UsernameNotFoundException(USER_EMAIL_NOT_FOUND_MESSAGE.formatted(email)));
     }
     
     public void checkExistenceByEmail(@NonNull String email) {
