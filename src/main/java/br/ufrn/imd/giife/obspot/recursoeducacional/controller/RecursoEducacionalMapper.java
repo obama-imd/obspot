7b35e61cd56e2fad200c7706dd23caa859a9934e
Emanuel Kywal <emanuelkywal@outlook.com>
2025-09-09 16:21:12 -0300

    feat: RecursoEducacionalMapper.mapper

--- src/main/java/br/ufrn/imd/giife/obspot/recursoeducacional/controller/RecursoEducacionalMapper.java
+++ src/main/java/br/ufrn/imd/giife/obspot/recursoeducacional/controller/RecursoEducacionalMapper.java
@@ -1,12 +1,18 @@
 package br.ufrn.imd.giife.obspot.recursoeducacional.controller;
 
 import br.ufrn.imd.giife.obspot.recursoeducacional.controller.dto.RecursoEducacionalRequestDTO;
+import br.ufrn.imd.giife.obspot.recursoeducacional.controller.dto.RecursoEducacionalUpdateRequestDTO;
 import br.ufrn.imd.giife.obspot.recursoeducacional.model.RecursoEducacionalEntity;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface RecursoEducacionalMapper {
 
     RecursoEducacionalEntity toEntity(RecursoEducacionalRequestDTO requestDTO);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    RecursoEducacionalEntity update(RecursoEducacionalUpdateRequestDTO updateDTO, @MappingTarget RecursoEducacionalEntity recurso);
 }
