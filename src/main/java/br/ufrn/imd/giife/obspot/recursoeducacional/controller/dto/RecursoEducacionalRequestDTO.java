f6055486efe3fc1361b09c88a4186ab1918a73d5
Emanuel Kywal <emanuelkywal@outlook.com>
2025-09-09 15:35:36 -0300

    feat: DTO RecursoEducacionalRequestDTO

---
+++ src/main/java/br/ufrn/imd/giife/obspot/recursoeducacional/controller/dto/RecursoEducacionalRequestDTO.java
@@ -0,0 +1,51 @@
+package br.ufrn.imd.giife.obspot.recursoeducacional.controller.dto;
+
+import br.ufrn.imd.giife.obspot.recursoeducacional.model.TipoRecursoEducacional;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+public record RecursoEducacionalRequestDTO(
+        @Schema(
+                description = "Título do recurso educacional.",
+                example = "Introdução à Biologia Celular"
+        )
+        String title,
+
+        @Schema(
+                description = "Breve descrição sobre o conteúdo do recurso.",
+                example = "Um vídeo interativo sobre as organelas celulares."
+        )
+        String description,
+
+        @Schema(
+                description = "Caminho ou URL para o arquivo do recurso.",
+                example = "/uploads/videos/biologia_celular.pdf"
+        )
+        String pathFile,
+
+        @Schema(
+                description = "Lista de IDs dos autores principais do recurso.",
+                example = "[1, 2]"
+        )
+        List<Long> authorsIds,
+
+        @Schema(
+                description = "Lista de IDs dos co-autores que contribuíram com o recurso.",
+                example = "[3]"
+        )
+        List<Long> coAuthorsIds,
+
+        @Schema(
+                description = "Lista de IDs dos orientadores do desenvolvimento do recurso.",
+                example = "[4, 5]"
+        )
+        List<Long> teachersIds,
+
+        @Schema(
+                description = "Tipo do recurso educacional.",
+                example = "ARTIGO"
+        )
+        TipoRecursoEducacional tipoRecursoEducacional
+) {
+}
